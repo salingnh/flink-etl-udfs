04466d5b2681efba66af9b44746b153a5a89e8e0
SangNguyen <sangnvnkl@gmail.com>
2026-08-11 10:19:50 +0700

    style: fix enrichment wrapper import ordering

--- src/flink_etl_udfs/udfs/enrichment.py
+++ src/flink_etl_udfs/udfs/enrichment.py
@@ -2,9 +2,8 @@
 
 from __future__ import annotations
 
-from pyflink.table.udf import udf
-
 from flink_etl_udfs.enrichment.profile import extract_profile_url_value
+from pyflink.table.udf import udf
 
 
 # REST enrichment là nondeterministic và phải chạy dưới async scalar UDF của Flink.
