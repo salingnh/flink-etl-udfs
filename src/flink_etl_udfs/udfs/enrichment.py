903a787918dd12e87f74a9c4702d80e3a38aa3dc
SangNguyen <sangnvnkl@gmail.com>
2026-08-11 15:50:43 +0700

    style: normalize enrichment import spacing

--- src/flink_etl_udfs/udfs/enrichment.py
+++ src/flink_etl_udfs/udfs/enrichment.py
@@ -1,8 +1,8 @@
 """PyFlink scalar UDF wrappers for external enrichment services."""
 
 from pyflink.table.udf import udf
-from flink_etl_udfs.enrichment.profile import extract_profile_url_sync
 
+from flink_etl_udfs.enrichment.profile import extract_profile_url_sync
 
 # REST enrichment là nondeterministic nhưng dùng scalar UDF sync trực tiếp để tương thích SQL Gateway 2.2.x.
 extract_profile_url = udf(
