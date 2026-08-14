4cdbc44a9e03812dac08ae88eee17ae2db2de3ef
SangNguyen <sangnvnkl@gmail.com>
2026-08-15 00:02:06 +0700

    refactor: isolate curated generic UDFs

---
+++ src/flink_etl_udfs/udfs/generic.py
@@ -0,0 +1,48 @@
+"""Curated generic PyFlink UDFs that are not attributed to one external standard."""
+
+from __future__ import annotations
+
+from pyflink.table.udf import udf
+
+from flink_etl_udfs.core import common
+
+
+def _string_udf(function):
+    return udf(function, input_types=["STRING"], result_type="STRING", deterministic=True)
+
+
+canonicalize_json = _string_udf(common.canonicalize_json_value)
+flatten_json = _string_udf(common.flatten_json_value)
+latin_name_search_key = _string_udf(common.latin_name_search_key_value)
+normalize_address_text = _string_udf(common.normalize_address_text_value)
+normalize_decimal = _string_udf(common.normalize_decimal_value)
+normalize_identifier_code = _string_udf(common.normalize_identifier_code_value)
+normalize_null_token = _string_udf(common.normalize_null_token_value)
+normalize_person_name = _string_udf(common.normalize_person_name_value)
+
+is_valid_json = udf(
+    common.is_valid_json_value,
+    input_types=["STRING"],
+    result_type="BOOLEAN",
+    deterministic=True,
+)
+stable_record_id = udf(
+    common.stable_record_id_value,
+    input_types=["STRING", "STRING"],
+    result_type="STRING",
+    deterministic=True,
+)
+
+
+__all__ = [
+    "canonicalize_json",
+    "flatten_json",
+    "is_valid_json",
+    "latin_name_search_key",
+    "normalize_address_text",
+    "normalize_decimal",
+    "normalize_identifier_code",
+    "normalize_null_token",
+    "normalize_person_name",
+    "stable_record_id",
+]
