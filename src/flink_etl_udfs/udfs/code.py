8603a8c61b9f7e6bc7decb049a20e3a608e15045
SangNguyen <sangnvnkl@gmail.com>
2026-08-15 07:12:29 +0700

    refactor: cast code UDF inputs inside Python

--- src/flink_etl_udfs/udfs/code.py
+++ src/flink_etl_udfs/udfs/code.py
@@ -7,7 +7,7 @@ from flink_etl_udfs.udfs._safe import try_udf
 
 normalize_repository_url = try_udf(
     normalize_repository_url_value,
-    input_types=["STRING"],
+    cast_types=["STRING"],
     result_type="STRING",
     deterministic=True,
 )
